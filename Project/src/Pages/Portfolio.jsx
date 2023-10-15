932a1f780b4838c027b9b7b670df26b0a2909074
oyeyemiife <ifeoluwaoyeyemi915@gmail.com>
2023-10-15 16:33:38 +0100

    Portfolio

--- Project/src/Pages/Portfolio.jsx
+++ Project/src/Pages/Portfolio.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./Portfolio.css"
 import tip from "../Images/Tip-Calculator.png"
-import age from "../Images/age.png"
+// import age from "../Images/age.png"
 import faq from "../Images/faq.png"
+
 {/* <script src="http://localhost:8097"></script> */}
 
 
@@ -18,13 +19,13 @@ const Portfolio = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At esse beatae nihil sequi cupiditate neque nobis voluptatum placeat veritatis assumenda? Fuga omnis placeat doloremque debitis, nemo nostrum ex illum culpa.</p>
             </div>
           </div>
-          <div className="work">
+          {/* <div className="work">
             <img src={age} alt="" />
             <div className="layer">
               <h3>Age Calculator</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At esse beatae nihil sequi cupiditate neque nobis voluptatum placeat veritatis assumenda? Fuga omnis placeat doloremque debitis, nemo nostrum ex illum culpa.</p>
             </div>
-          </div>
+          </div> */}
           <div className="work">
             <img src={faq} alt="" />
             <div className="layer">
